cebca94acb33ebd0fb68d1aba889c90bcabdadcc
Andy Kolean <andy@optimize.fitness>
2019-07-19 14:23:47 -0400

    fix defaultListCell constant max width

--- Source/Cells/DefaultListCell.swift
+++ Source/Cells/DefaultListCell.swift
@@ -261,7 +261,7 @@ extension DefaultListCell {
     containerCenterXConstraint?.isActive = true
 
     maxContainerWidthConstraint = containerView.widthAnchor.constraint(
-      lessThanOrEqualToConstant: 600
+      lessThanOrEqualToConstant: contentView.readableContentGuide.widthAnchor
     )
     maxContainerWidthConstraint?.isActive = true
 
